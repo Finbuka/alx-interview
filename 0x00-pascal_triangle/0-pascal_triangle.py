7759d79da493b7f698e2af7916190416bd1c5e4f
Finbuka <ebukajoe100@gmail.com>
2023-01-26 03:41:58 +0100

    changed tab

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,26 @@
-"""
-Module that returns a list of lists
-of integers representing the Pascal's
-triangle of n
-"""
+#!/usr/bin/python3
+"""alx interview question on Pascal's Triangle"""
+
 
 def pascal_triangle(n):
-	"""
-	Function that returns a list of lists
-	of integers representing the Pascal's
-	triangle of n
-	"""
-	triangle = [[1]]
-	if n <= 0:
-		return [[]]
-	else:
-		for _ in range(1,n):
-			temp = [1]
-			for i in range(len(triangle[-1])):
-				if i+1 < len(triangle[-1]):
-					temp.append(triangle[-1][i] + triangle[-1][1+i])
-			temp.append(1)
-			triangle.append(temp)
-		return triangle
+    """
+      returns a list of lists of integers
+      representing the Pascal’s triangle of n
+    Args:
+      n (n): size of triangle
+    Returns:
+      list: returns empty list if n <= 0 or list of lists of integers
+      representing Pascal's triangle of n otherwise
+    """
+    triangle = [[1]]
+    if n <= 0:
+        return [[]]
+    else:
+        for _ in range(1, n):
+            temp = [1]
+            for i in range(len(triangle[-1])):
+                if i + 1 < len(triangle[-1]):
+                    temp.append(triangle[-1][i] + triangle[-1][1 + i])
+            temp.append(1)
+            triangle.append(temp)
+        return triangle
