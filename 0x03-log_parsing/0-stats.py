ed0de017e08507a408da7681259fbd7b07a91ee5
Finbuka <ebukajoe100@gmail.com>
2023-02-20 01:36:46 +0100

    docs

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,32 +4,7 @@ A script: Reads standard input line by line and computes metrics
 """
 
 
-def signal_handler(sig, frame, total_file_size, stat_cod_cnt):
-    """
-    Handles the CTRL + c signal
-    Args:
-        sig (int): total log size after every 10 successfully read line
-        frame (FrameType): dictionary of status codes and counts
-        total_file_size (int): params for the print stats
-        stat_cod_cnt (int): params for the print stats
-    """
-    print_stats(total_file_size, stat_cod_cnt)
-    sys.exit(0)
-
-
-def print_stats(total_file_size, stat_cod_cnt):
-    """
-    Prints generated report to standard output
-    Args:
-        total_file_size (int): total log size after every 10 successfully
-        stat_cod_cnt (dict): dictionary of status codes and counts
-    """
-    print(f"File size: {total_file_size[0]}")
-    for status_code in sorted(stat_cod_cnt.keys()):
-        print(f"{status_code}: {stat_cod_cnt[status_code]}")
-
-
-def parse_logs():
+def parseLogs():
     """
     Reads logs from standard input and generates reports
     Reports:
@@ -37,38 +12,44 @@ def parse_logs():
     Raises:
         KeyboardInterrupt (Exception): handles this exception and raises it
     """
-    total_file_size = [
-        0,
-    ]
-    stat_cod_cnt = {}
-    line_num = 0
-    new_signal_handler = functools.partial(
-        signal_handler,
-        total_file_size=total_file_size,
-        stat_cod_cnt=stat_cod_cnt,
-    )
-    signal.signal(signal.SIGINT, new_signal_handler)
-
-    for line in sys.stdin:
-        line_num += 1
-
-        try:
-            _, _, _, _, _, _, _, status_code, file_size = line.split()
-            status_code = int(status_code)
-            file_size = int(file_size)
-        except ValueError:
-            continue
-
-        total_file_size[0] += file_size
-        stat_cod_cnt[status_code] = stat_cod_cnt.get(status_code, 0) + 1
-
-        if line_num % 10 == 0:
-            print_stats(total_file_size, stat_cod_cnt)
+    stdin = __import__('sys').stdin
+    lineNumber = 0
+    fileSize = 0
+    statusCodes = {}
+    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    try:
+        for line in stdin:
+            lineNumber += 1
+            line = line.split()
+            try:
+                fileSize += int(line[-1])
+                if line[-2] in codes:
+                    try:
+                        statusCodes[line[-2]] += 1
+                    except KeyError:
+                        statusCodes[line[-2]] = 1
+            except (IndexError, ValueError):
+                pass
+            if lineNumber == 10:
+                report(fileSize, statusCodes)
+                lineNumber = 0
+        report(fileSize, statusCodes)
+    except KeyboardInterrupt as e:
+        report(fileSize, statusCodes)
+        raise
 
 
-if __name__ == "__main__":
-    import sys
-    import signal
-    import functools
+def report(fileSize, statusCodes):
+    """
+    Prints generated report to standard output
+    Args:
+        fileSize (int): total log size after every 10 successfully read line
+        statusCodes (dict): dictionary of status codes and counts
+    """
+    print("File size: {}".format(fileSize))
+    for key, value in sorted(statusCodes.items()):
+        print("{}: {}".format(key, value))
 
-    parse_logs()
+
+if __name__ == '__main__':
+    parseLogs()
